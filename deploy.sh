a1444f583d963d7c07eb5230cc25e8a3cceda003
Dustin Sinkey <dustin@cherre.com>
2022-08-28 12:15:29 -0300

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t dsinkey9/multi-client:latest -t dsinkey9/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t dsinkey9/multi-server:latest -t dsinkey9/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t dsinkey9/multi-worker:latest -t dsinkey9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push dsinkey9/multi-client:latest
+docker push dsinkey9/multi-server:latest
+docker push dsinkey9/multi-worker:latest
+
+docker push dsinkey9/multi-client:$SHA
+docker push dsinkey9/multi-server:$SHA
+docker push dsinkey9/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=dsinkey9/multi-server:$SHA
+kubectl set image deployments/client-deployments server=dsinkey9/multi-client:$SHA
+kubectl set image deployments/worker-deployments server=dsinkey9/multi-worker:$SHA
